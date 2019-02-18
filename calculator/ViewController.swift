eaf64b58c0fb991d22521c7a750491ac36b816d0
Tara Jue <tjue@professorX.highpoint.edu>
2019-02-18 13:49:47 -0500

    Attempted the app, displays question

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -9,16 +9,61 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var firstNum: UILabel!
+    @IBOutlet var secondNum: UILabel!
+    @IBOutlet var signLabel: UILabel!
+    @IBOutlet var scoreLabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
+    
+    var currAnswerIndex: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        firstNum.text = firstNums[currAnswerIndex]
+        secondNum.text = secondNums[currAnswerIndex]
+        signLabel.text = signs[currAnswerIndex]
     }
+    
+    let firstNums: [String] = [
+        "5","2","3","4","6","8","9","3","4","7"
+    ]
+    
+    let signs: [String] = [
+        "+","-","*","/","+","-","*","/","+","-"
+    ]
+    
+    let secondNums: [String] = [
+        
+        "1","2","3","4", "4", "0", "0", "1", "4", "9"
+    ]
+    
+    let answers: [String] = [
+        "6","0","9","1","10","8","0","3","8","-2"
+    ]
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    var total: Int = 0
+    var correct: Int = 0
+
+    @IBAction func nextQuestion(_ sender: UIButton)
+    {
+        let answer = answerLabel.text
+        print(answer)
+        if answer == answers[currAnswerIndex] {
+            correct+=1
+        }
+        
+        currAnswerIndex+=1
+        if currAnswerIndex == firstNums.count {
+            currAnswerIndex=0
+        }
+        
+        firstNum.text = firstNums[currAnswerIndex]
+        secondNum.text = secondNums[currAnswerIndex]
+        signLabel.text = signs[currAnswerIndex]
+        scoreLabel.text = "\(correct) / 10"
     }
+    
 
 
 }
