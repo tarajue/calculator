441840a086458fedfc6a36e536e1e8bf6496570d
Tara Jue <tjue@i146>
2019-02-25 15:07:46 -0500

    made a lot of changes

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
     
@@ -14,42 +15,76 @@ class ViewController: UIViewController {
     @IBOutlet var secondNum: UILabel!
     @IBOutlet var signLabel: UILabel!
     @IBOutlet var scoreLabel: UILabel!
-    @IBOutlet var answerLabel: UILabel!
+    @IBOutlet var answerLabel: UITextField!
     
     var currAnswerIndex: Int = 0
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        firstNum.text = firstNums[currAnswerIndex]
-        secondNum.text = secondNums[currAnswerIndex]
-        signLabel.text = signs[currAnswerIndex]
+    func random(max MaxNumber:Int)-> String {
+        return String(arc4random_uniform(UInt32(MaxNumber)))
     }
     
-    let firstNums: [String] = [
-        "5","2","3","4","6","8","9","3","4","7"
-    ]
     
     let signs: [String] = [
-        "+","-","*","/","+","-","*","/","+","-"
+        "+","-","*","/"
     ]
     
-    let secondNums: [String] = [
-        
-        "1","2","3","4", "4", "0", "0", "1", "4", "9"
-    ]
     
-    let answers: [String] = [
-        "6","0","9","1","10","8","0","3","8","-2"
-    ]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        firstNum.text = random(max: 10)
+        secondNum.text = random(max: 10)
+        signLabel.text = String(arc4random_uniform(UInt32(signs.count)))
+    }
+    
+    func add(_ a: Int, _ b: Int)->Int
+    {
+        return a+b
+    }
+    
+    func sub(_ a: Int, _ b: Int)->Int
+    {
+        return a-b
+    }
+    
+    func mult(_ a: Int, _ b: Int)->Int
+    {
+        return a*b
+    }
+    
+    func div(_ a: Int, _ b: Int)->Int
+    {
+        if b != 0 {
+            return a/b
+        }   else {
+            return 0
+        }
+    }
+    
+    func doMath(_ a: Int, _ b: Int, _ op:Character) -> Int
+    {
+        switch op {
+            case "+": return add(a,b)
+            case "-": return sub(a,b)
+            case "*": return mult(a,b)
+            case "/": return div(a,b)
+            default: return 0
+        }
+    }
 
+    
     var total: Int = 0
     var correct: Int = 0
+    
+    @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer) {
+        answerLabel.resignFirstResponder()
+    }
 
     @IBAction func nextQuestion(_ sender: UIButton)
     {
         let answer = answerLabel.text
-        print(answer)
-        if answer == answers[currAnswerIndex] {
+        let correctAnswer = doMath(firstNum.text,secondNum.text,signLabel.text)
+        //print(answer)
+        if answer == correctAnswer {
             correct+=1
         }
         
@@ -61,6 +96,7 @@ class ViewController: UIViewController {
         firstNum.text = firstNums[currAnswerIndex]
         secondNum.text = secondNums[currAnswerIndex]
         signLabel.text = signs[currAnswerIndex]
+        answerLabel.text = ""
         scoreLabel.text = "\(correct) / 10"
     }
     
